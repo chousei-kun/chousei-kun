8005fbf866b39b42f7167d08e819b6f53f98f409
waku0417 <waku0417@gmail.com>
2026-06-08 19:41:37 +0900

    Set notification webhook url

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.SLOTWISE_CONFIG = {
   googleClientId: "607710624005-hqja9n0ckvhvc55arfqmj2649vpt6ac9.apps.googleusercontent.com",
   roomStore: "local",
   preferredGoogleAccount: "waku0417@gmail.com",
-  notificationWebhookUrl: ""
+  notificationWebhookUrl: "https://script.google.com/macros/s/AKfycbwMcRhsBqC4hB4M6nA6c0AUJDhke1cAlpxot6zc5wSgiE869DoWg3WJsdapVmQYWypEKA/exec"
 };
